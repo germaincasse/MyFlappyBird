0020f38079e7064e14fc582f6b178967a7578496
Germain Cassé <germain.casse@epita.fr>
2020-06-17 17:36:16 +0200

    everything done

---
+++ Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMovement : MonoBehaviour
+{
+    public GameManager gameManager;
+    public GameObject leftWall;
+    public float velocity = 1;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0)
+            || (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)) {
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject == leftWall)
+            gameManager.GameOver();
+    }
+}
